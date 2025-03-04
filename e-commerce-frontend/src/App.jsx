2636bc2f33f83e9b37b07d843cb070d6f2840474
AllanKariuki <a.kariuki@keshiholdings.com>
2025-03-04 11:49:58 +0300

    chore: project initialization

---
+++ e-commerce-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='bg-red-500 fles justify-center items-center h-screen'>
+        <h2 className='text-white text-5xl font-bold'>Hello World</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
