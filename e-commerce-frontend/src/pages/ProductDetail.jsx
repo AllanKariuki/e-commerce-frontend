e09199cdf00bdfdb380403b1ab021104dcd5590a
AllanKariuki <a.kariuki@keshiholdings.com>
2025-05-09 13:34:23 +0300

    refactor: screen width redefinition

--- e-commerce-frontend/src/pages/ProductDetail.jsx
+++ e-commerce-frontend/src/pages/ProductDetail.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import { useParams } from 'react-router-dom';
 import { Heart, ShoppingCart, ChevronDown, Star, ChevronDownCircle, ChevronRightCircle, ChevronLeftCircle } from 'lucide-react';
 import ProductImages from "../components/product-details/ProductImages";
+import ProductDetails from '../components/product-details/ProductDetails';
+import ProductTabs from '../components/product-details/ProductTabs';
+import RelatedProductsCarousel from '../components/product-details/RelatedProductsCarousel';
 
 const ProductDetail = () => {
     const { id } = useParams();
     
     return (
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+        <div className="max-w-5/6 mx-auto px-4 sm:px-6 lg:px-8 py-10">
             <div className="py-4">
 
             <div className="text-sm text-gray-500 mb-2">
@@ -20,7 +23,7 @@ const ProductDetail = () => {
                 </nav>
             </div>
                 
-                <div className="grid grid-cols-1 lg-cols-2 gap-8 mt-4">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-4">
                     <ProductImages />
                     <ProductDetails />
                 </div>
