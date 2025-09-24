897227d0c19422463f9c649270b92e985433dd0d
AllanKariuki <a.kariuki@keshiholdings.com>
2025-09-24 12:25:08 +0300

    feat: add image extensions declaration to fix image imports

--- e-commerce-frontend/vite-env.d.ts
+++ e-commerce-frontend/vite-env.d.ts
@@ -4,3 +4,13 @@ declare module "*.css";
 declare module "*.scss";
 declare module "*.sass";
 declare module "*.less";
+
+// Image file types
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.gif";
+declare module "*.svg";
+declare module "*.webp";
+declare module "*.bmp";
+declare module "*.tiff";
