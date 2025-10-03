48fa79657aa3167e3d6fc1da0e7cbed1c5c9e36f
AllanKariuki <a.kariuki@keshiholdings.com>
2025-10-03 12:42:15 +0300

    feat: add config type defintion and reference

--- e-commerce-frontend/src/api/axios.ts
+++ e-commerce-frontend/src/api/axios.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { getConfig } from '../Config';
-import type { AxiosResponse, AxiosError } from 'axios';
+import type { AxiosResponse, AxiosError, InternalAxiosRequestConfig } from 'axios';
 
 const { VITE_API_BASE_URL } = getConfig();
+
 const axiosInstance = axios.create({
-    baseURL: VITE_API_BASE_URL || 'http://localhost:8080/api',
+    baseURL: VITE_API_BASE_URL || 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json'
     },
@@ -12,7 +13,7 @@ const axiosInstance = axios.create({
 
 const setupInterceptors = () => {
     axiosInstance.interceptors.request.use( 
-        (config: any) => {
+        (config: InternalAxiosRequestConfig) => {
         if (window.location.href.includes('orders')) {
             const token = getToken();
             if (token) {
@@ -25,11 +26,14 @@ const setupInterceptors = () => {
         return Promise.reject(error);
     });
 
-    axiosInstance.interceptors.response.use((response: AxiosResponse) => {
-        return response.data;
-    }, (error: AxiosError) => {
-        return Promise.reject(error);
-    })
+    axiosInstance.interceptors.response.use(
+        (response: AxiosResponse) => {
+            return response.data;
+        }, 
+        (error: AxiosError) => {
+            return Promise.reject(error);
+        }
+    );
 }
 
 export default axiosInstance;
