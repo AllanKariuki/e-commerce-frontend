4ddd3530a630c472f90cab199377931789813048
AllanKariuki <a.kariuki@keshiholdings.com>
2025-10-19 15:45:31 +0300

    feat: setup conditional render of loading message, no products message and product filter props

--- e-commerce-frontend/src/pages/Products.tsx
+++ e-commerce-frontend/src/pages/Products.tsx
@@ -3,33 +3,60 @@ import ProductDetailCard from "../components/ProductDetailCard";
 import RecentFinds from "../components/product/RecentFinds";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch } from "../redux/store";
-import { fetchProducts, selectProducts, selectTotalProducts } from "../redux/slices/productsSlice";
-import { useEffect } from "react";
+import { fetchProducts, selectProducts, selectTotalProducts, selectIsLoading } from "../redux/slices/productsSlice";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Products = () => {
     const dispatch =  useDispatch<AppDispatch>();
     const products = useSelector(selectProducts);
     const totalProducts = useSelector(selectTotalProducts);
+    const loading = useSelector(selectIsLoading);
+    const [searchParams] = useSearchParams();
+    const categoryName = searchParams.get('category-name');
+    const [minPrice, setMinPrice] = useState<number | undefined>();
+    const [maxPrice, setMaxPrice] = useState<number | undefined>();
+    const [sizeFilter, setSizeFilter] = useState<string | undefined>();
+    const [ratingsFilter, setRatingsFilter] = useState<number | undefined>();
 
+    console.log("Category name: ", categoryName);
     useEffect(() => {
+        if (categoryName) {
+            dispatch(fetchProducts({ category: categoryName.toLowerCase() }) as any);
+            return;
+        }
         dispatch(fetchProducts as any);
-    }, [dispatch]);
+    }, [dispatch, categoryName]);
 
     return (
         <div className="">
             <h2 className="text-5xl text-center font-semibold my-6">Products Page</h2>
             <div className="max-w-5/6 grid grid-cols-1 lg:grid-cols-4 mx-auto p-6">
                 <div className="hidden xl:block">
-                    <ProductsFilter />
+                    <ProductsFilter 
+                        minPrice={minPrice}
+                        setMinPrice={setMinPrice}
+                        maxPrice={maxPrice}
+                        setMaxPrice={setMaxPrice}
+                        sizeFilter={sizeFilter}
+                        setSizeFilter={setSizeFilter}
+                        ratingsFilter={ratingsFilter}
+                        setRatingsFilter={setRatingsFilter}
+                    />
                 </div>
                 
                 <div className="lg:grid lg:col-span-3 px-6">
-                    
-                    <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 my-8">
-                        {products.map((product, i) => (
-                            <ProductDetailCard product={product} key={i} />
-                        ))}
-                    </div>
+                    { loading ? (
+                        <p className="text-center text-gray-500 mt-20">Loading products...</p>
+                    ) : products.length < 1 ? (
+                        <p className="text-center text-gray-500 mt-20">No products found.</p>
+                    ) : (
+                        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 my-8">
+                            {products.map((product, i) => (
+                                <ProductDetailCard product={product} key={i} />
+                            ))}
+                        </div>
+                    )}
 
                     {/* Recent Finds Component */}
                     <RecentFinds />
