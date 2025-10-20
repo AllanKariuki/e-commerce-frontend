e657c9d1a39f707310398b24d5ed543529299e54
AllanKariuki <a.kariuki@keshiholdings.com>
2025-10-20 16:00:01 +0300

    feat: recent finds slice to handle the interaction with the api

---
+++ e-commerce-frontend/src/redux/slices/recentViewsSlice.ts
@@ -0,0 +1,63 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { Product, ProductsResponse } from "../../types/product";
+import type { RecentViewState } from "../../types/recentViews";
+import { get } from "../../api";
+
+const intialState: RecentViewState = {
+    items: [],
+    loading: false,
+    error: null,
+};
+
+export const fetchRecentViews = createAsyncThunk(
+    'recentViews/fetchRecentViews',
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await get<ProductsResponse>('/products/recent/');
+            return response;
+        } catch (error: any) {
+            return rejectWithValue(error.message);
+        }
+    }
+)
+
+const recentViewsSlice = createSlice({
+    name: 'recentViews',
+    initialState: intialState,
+    reducers: {
+        clearRecentViews: (state) => {
+            state.items = [];
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchRecentViews.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(fetchRecentViews.fulfilled, (state, action: PayloadAction<ProductsResponse>) => {
+                state.loading = false;
+                state.items = action.payload.products;
+            })
+            .addCase(fetchRecentViews.rejected, (state, action: PayloadAction<any>) => {
+                state.loading = false,
+                state.error = action.payload;
+            })
+    }
+})
+
+export const { clearRecentViews } = recentViewsSlice.actions;
+
+export const selectRecentProducts = (state: { recentViews: RecentViewState }): Product[] =>  {
+    return state.recentViews?.items || [];
+};
+
+export const selectLoading = (state: { recentViews: RecentViewState }): boolean => {
+    return state.recentViews.loading;
+};
+
+export const selectFetchError = (state: { recentViews: RecentViewState }):  string | null => {
+    return state.recentViews.error;
+}
+
+export default recentViewsSlice.reducer;
