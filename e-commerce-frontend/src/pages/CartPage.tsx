7ba1d95fd1418b0248d0cc101344fe09cec72b52
AllanKariuki <a.kariuki@keshiholdings.com>
2025-10-21 12:04:24 +0300

    feat: addition of error, loading and empty cart sections

--- e-commerce-frontend/src/pages/CartPage.tsx
+++ e-commerce-frontend/src/pages/CartPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Trash2, ShoppingCart, Check, ChevronRight, Plus, Minus } from 'lucide-react';
+import { Trash2, ShoppingCart, Check, ChevronRight, Plus, Minus, ShoppingBag } from 'lucide-react';
 import CartItem from '../components/cart/CartItem';
 import CheckoutSteps from '../components/cart/CheckoutSteps';
 import OrderSummary from '../components/cart/OrderSummary';
@@ -7,9 +7,11 @@ import type { CartItemType } from '../types/cart';
 import { useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch } from '../redux/store';
 import { cartDeliveryFee, cartDiscount, cartError, cartIsLoading, cartItems, cartPercentageDiscount, cartSubtotal, cartTotalAmount, removeItemFromCart, updateItemQuantity } from '../redux/slices/cartSlice';
+import { useNavigate } from 'react-router-dom';
 
 const CartPage = () => {
        const dispatch = useDispatch<AppDispatch>();
+       const navigate = useNavigate();
        const isLoading = useSelector(cartIsLoading);
        const error = useSelector(cartError);
        const cartList = useSelector(cartItems);
@@ -60,52 +62,70 @@ const CartPage = () => {
             {/* Cart Items Section */}
             <div className="grid col-span-2">
               <h2 className="text-xl font-medium mb-4">Your cart</h2>
-              
-              <div 
-                className="flex 
-                    justify-between 
-                    items-center 
-                    mb-4 pb-2 p-3
-                    border-1 
-                    border-gray-200 
-                    rounded-full"
-                >
-                  <label className="flex items-center">
-                    <input 
-                        type="checkbox" 
-                        checked={selectAll} 
-                        onChange={() => setSelectAll(!selectAll)}
-                        className="mr-2"
-                    />
-                    <span>Select All</span>
-                  </label>
-                  <button 
-                    onClick={deleteSelectedItems}
-                    className="bg-black
-                    text-white 
-                    py-3 px-5 rounded-3xl text-sm flex 
-                    items-center"
+              {
+                isLoading ? (
+                  <div className="text-center py-20 text-lg">Loading cart items...</div>
+                ) : error ? (
+                  <div className="text-center py-20 text-lg text-red-500">Error loading cart items: {error}</div>
+                ) : cartList.length === 0 ? (
+                  <div className="flex flex-col items-center justify-center h-64 text-gray-500">
+                    <ShoppingCart size={48} className="mb-4" />
+                    <p className="text-lg">Your cart is currently empty.</p>
+                    <button
+                        onClick={() => navigate('/products')}
+                        className="flex items-center gap-2 bg-black text-white px-6 py-3 my-3 rounded-lg hover:bg-gray-800 transition-colors duration-200 cursor-pointer"
                     >
-                    Delete
-                  </button>
-              </div>
-
-              <div className="bg-white rounded-lg shadow-md p-4">
-                {/* Header with select all and delete */}
+                        <ShoppingBag size={20} />
+                        Start Shopping
+                    </button>
+                  </div>
+                ) : (
+                  <div className="bg-white rounded-lg shadow-md p-4">
+                    {/* Header with select all and delete */}
+                    <div
+                      className="flex 
+                          justify-between 
+                          items-center 
+                          mb-4 h-16 pb-2 p-3
+                          border-1 
+                          border-gray-200 
+                          rounded-full "
+                      >
+                        <label className="flex items-center">
+                          <input 
+                              type="checkbox" 
+                              checked={selectAll} 
+                              onChange={() => setSelectAll(!selectAll)}
+                              className="mr-2"
+                          />
+                          <span>Select All</span>
+                        </label>
+                        <button 
+                          onClick={deleteSelectedItems}
+                          className="bg-black
+                          text-white 
+                          py-3 px-5 rounded-3xl text-sm flex 
+                          items-center"
+                          >
+                          Delete
+                        </button>
+                    </div>
+                    
+                    {/* Cart Items */}
+                    <div className="space-y-4">
+                      {cartList.map(item => (
+                        <CartItem 
+                          key={item.item.id} 
+                          item={item} 
+                          updateQuantity={updateQuantity}
+                          removeItem={handleRemoveItem}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )
+              }
                 
-                
-                {/* Cart Items */}
-                <div className="space-y-4">
-                  {cartList.map(item => (
-                    <CartItem 
-                      key={item.item.id} 
-                      item={item} 
-                      updateQuantity={updateQuantity}
-                      removeItem={handleRemoveItem}
-                    />
-                  ))}
-                </div>
-              </div>
             </div>
             
             {/* Order Summary Section */}
